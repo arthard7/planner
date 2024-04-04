870974ed64d5fe9c694a72cbfcd400dccb2d1ff7
Artem Chuprina <ar.crrr@yandex.ru>
2024-04-04 18:10:30 +0300

    based settings

---
+++ planner-front/config/pages-url.config.ts
@@ -0,0 +1,13 @@
+class DASHBOARD {
+    private root = '/i'
+
+    HOME = this.root
+    TASKS = `${this.root}/tasks`
+    HABITS = `${this.root}/habits`
+    TIMER = `${this.root}/timer`
+    TIMER_BLOCKING = `${this.root}/time-blocking`
+    SETTINGS = `${this.root}/settings`
+
+}
+
+export const DASHBOARDS_PAGES = new DASHBOARD()
